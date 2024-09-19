9b3992fb42f5a9fecc3bc8ef3a32afd9e4e7f3e9
minhi0449 <minhi0449@gmail.com>
2024-09-19 15:28:01 +0900

    github action

---
+++ src/main/java/com/minhee/SpringGithubActionAppApplication.java
@@ -0,0 +1,28 @@
+package com.minhee;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringGithubActionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGithubActionAppApplication.class, args);
+    }
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+
+    @GetMapping(value = {"/", "index"})
+    public String index() {
+        return appName + " " + appVersion;
+    }
+
+}
